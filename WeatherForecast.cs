5ea7dbfd0e2ec83f2e87bb395973e504da468f3a
F3rcho111 <dfci_17@hotmail.com>
2022-10-26 17:52:35 -0500

    Declaramos un Bool True

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -9,7 +9,7 @@ namespace Proyecto_Entrevista
         public int TemperatureF => 15 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        
-        public bool MiFer { get; set; }
+
+        public bool MiFer  { get; set; }
     }
 }
